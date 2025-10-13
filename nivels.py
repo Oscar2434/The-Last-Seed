7bddb43ca7f08887b99237f22a41867be9a223ca
Oscar <angelmoises549@gmail.com>
2025-10-13 07:11:28 -0600

    implemento de play

--- nivels.py
+++ nivels.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import constants
 from button import Button
+import main  # Importar el archivo main para acceder a su funcionalidad
 
 pygame.init()
 
@@ -13,14 +14,22 @@ pygame.display.set_caption("nivels")
 Fondo = pygame.image.load("imagenes/portada.png")  # Cambia el nombre si es necesario
 Fondo = pygame.transform.scale(Fondo, (constants.WIDTH, constants.HEIGHT))
 
+# Botón de Play
+button_play = pygame.image.load("imagenes/Play.png")  # Usa la imagen proporcionada
+button_play = pygame.transform.scale(button_play, (300, 100))  # Escala la imagen
+play_button = Button(780 // 2 - button_play.get_width() // 2, 500 // 2 - button_play.get_height() // 2, button_play, 1)
+
 # Bucle de configuración
 def niveles():
     run = True
     while run:
         screen.blit(Fondo, (0, 0))
+        
+        # Dibujar el botón de Play
+        if play_button.draw(screen):
+            main.main()  # Llama a la función principal del archivo main.py
 
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
