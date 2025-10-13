738beb7bd317137bdea5a0da2b00460641d2d730
Oscar <angelmoises549@gmail.com>
2025-10-13 07:37:33 -0600

    agregue el cambio de idioma de nivels

--- nivels.py
+++ nivels.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import config
 import constants
 from button import Button
 import main  # Importar el archivo main para acceder a su funcionalidad
@@ -15,7 +16,10 @@ Fondo = pygame.image.load("imagenes/portada.png")  # Cambia el nombre si es nece
 Fondo = pygame.transform.scale(Fondo, (constants.WIDTH, constants.HEIGHT))
 
 # Botón de Play
-button_play = pygame.image.load("imagenes/Play.png")  # Usa la imagen proporcionada
+if config.lenguaje:
+    button_play = pygame.image.load("imagenes/Play.png")  # Usa la imagen proporcionada
+else:
+    button_play = pygame.image.load("imagenes\Jugar.png")  # Usa la imagen proporcionada
 button_play = pygame.transform.scale(button_play, (300, 100))  # Escala la imagen
 play_button = Button(780 // 2 - button_play.get_width() // 2, 700 // 2 - button_play.get_height() // 2, button_play, 1)
 
