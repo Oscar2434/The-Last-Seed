29aeebd319cfd8f401683219495c356b8fe72fb7
Angel <angelmoises549@gmail.com>
2025-11-11 19:09:05 -0600

    Laberinto beta

--- world_nivel2.py
+++ world_nivel2.py
@@ -27,9 +27,9 @@ class World:
     def create_resources(self):
         # POSICIONES FIJAS para los 3 recursos en el laberinto
         resource_positions = [
-            (250, 130, "composta"),
-            (170, 260, "agua"),      # Este usará WaterResource
-            (710, 425, "semillas")
+            (570, 50, "composta"),
+            (110, 100, "agua"),      # Este usará WaterResource
+            (610, 425, "semillas")
         ]
         
         self.resources.clear()
@@ -49,37 +49,73 @@ class World:
         
         # MUROS INTERNOS DEL LABERINTO
         internal_walls = [
-            (115, 80), (204, 80), (476, 80),
+            #(476, 80),
               
-             (120, 220), (156, 220), (220, 220), (284, 220)#, (358, 230)
+             (30, 220), (115, 220), (200, 220), 
             
         ]
         internal_walls_2 = [
-            (604, 80),
-            (204, 150), (476, 150), (545, 150),
-            (120, 302), (209, 302), (348, 302), (412, 302), (604, 302)
+            #604, 80),
+            (90, 150), 
+            (284, 220),
+            (600, 90), (530, 90),
+
+            (475, 220), (580, 220),
+
+            (80, 302), (199, 302),
+            (80, 382), (199, 382),  
+            (380, 382),
+            (564, 302), (604, 302),
+            (564, 382), (604, 382)
         ]
         internal_walls_3 = [
-            (150, 302), (360, 302), (540, 80), (530, 150), (689, 80)
+            (30, 80), (100, 80),
+            (570, 90), 
+            (274, 220),
+            (150, 302), #(360, 382), #(540, 80), 
+            (140, 382),
+            (530, 220),
+            (584, 302),
+            (584, 382),
+            #(689, 80)
         ]
         
         # MUROS (de izquierda a derecha) INTERNOS DEL LABERINTO
         vertical_con_final = [
-            (290, 80), (455, 80), 
-            (350, 220)
+            (240, 90),
+            (160, 80),
+            #(630, 100), 
+            (520, 30), 
+            (348, 250),(348, 302),
+            (470, 250),(470, 302),
+            
+            (564, 382),
         ]
+        vertical_con_final_2 = [
+            (160, 232),(664, 292),
+            ]
         vertical_walls_sin = [
-            (90, 80), (90, 140), (90, 200), (90, 260), (90, 320), (90, 340)
+            #(90, 80), (90, 140), (90, 200), (90, 260), (90, 320), 
         ]
         vertical_walls_sin_2 = [
-            (90, 380)
+            (90, 380),(90, 340),(348, 270),
         ]
+        Union_wall_vertical = [
+             (240, 30),
+             (348, 290), (470, 280)
+             #(79, 470)
+            ]
+        Union_wall_vertical_2 = [
+             
+             ]
 
         # AGREGAR MUROS INTERNOS
-        for wall in vertical_walls_sin_2:
+        for wall in vertical_con_final_2: 
             wall_positions.append((wall[0], wall[1], "vertical_con_final"))
         for wall in internal_walls:
             wall_positions.append((wall[0], wall[1], "horizontal_sin_final"))
+        #for wall in vertical_walls_sin_2:
+        #    wall_positions.append((wall[0], wall[1], "vertical_con_final"))
 
         for wall in internal_walls_2:
             self.walls.append(Wall(wall[0], wall[1], "horizontal_con_final", constants.WALL_SCALE))
@@ -87,7 +123,13 @@ class World:
             self.walls.append(Wall(wall[0], wall[1], "horizontal_sin_final", constants.WALL_SCALE))
         for wall in vertical_walls_sin:
             self.walls.append(Wall(wall[0], wall[1], "vertical_sin_final", constants.WALL_SCALE))
-        
+        for wall in Union_wall_vertical_2:
+            self.walls.append(Wall(wall[0], wall[1], "vertical_sin_final", constants.WALL_SCALE))
+        for wall in vertical_con_final:
+            self.walls.append(Wall(wall[0], wall[1], "vertical_con_final", constants.WALL_SCALE))
+        for wall in Union_wall_vertical:
+            self.walls.append(Wall(wall[0], wall[1], "vertical_sin_final", constants.WALL_SCALE))
+
         #------Inicio-------
         # Techo (y = 0) - línea horizontal superior
         for x in range(0, 780, 51):
@@ -105,17 +147,11 @@ class World:
         # Piso (y = 440) - línea horizontal inferior  
         for x in range(0, 780, 51):
             wall_positions.append((x, 460, "normal"))
-        #--------fin de muros--------#
-        
-        
         
         # Crear muros en las posiciones definidas CON ESCALA
         for x, y, wall_type in wall_positions:
             self.walls.append(Wall(x, y, wall_type, constants.WALL_SCALE))
-        for wall in vertical_con_final:
-            self.walls.append(Wall(wall[0], wall[1], "vertical_con_final", constants.WALL_SCALE))
-
-
+        #--------fin de muros--------#
         
 
     def draw(self, screen):
